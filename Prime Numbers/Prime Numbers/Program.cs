ed222a2738f211f6507ca6fa61577b180deba860
roshni1015 <roshniadatrao1110@gmail.com>
2022-05-26 12:50:23 +0530

    Prime Number in Range 0 to 1000

---
+++ Prime Numbers/Prime Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class primenumberto1000
+{
+
+    public static void Main(string[] args)
+    {
+
+        int n, i, m = 0, flag = 0;
+        Console.WriteLine("Enter the Number");
+        n = int.Parse(Console.ReadLine());
+        m = n / 2;
+        for (i = 2; i <= m; i++)
+        {
+            if (n % i == 0)
+            {
+                Console.WriteLine("Number is not Prime");
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0)
+            Console.WriteLine("Number is Prime");
+
+    }
+}
